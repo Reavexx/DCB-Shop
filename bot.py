fb2b747dc241915844182a130f1c4beb03770765
Dines Nimalthas <dines_nimalthas@sluz.ch>
2023-04-27 15:25:08 +0200

    added Black listed Words

--- bot.py
+++ bot.py
@@ -22,7 +22,24 @@ async def send_message(message, user_message, is_private):
     except Exception as e:
         print(e)
 
+    
 
+async def on_offend(msg, client):
+
+    block_words = ["white", "black", "http://", "https://"]
+    # If the message wasn't sent by the bot
+    if msg.author != client.user:
+
+        # Going through each blocked word to check if it's in the message
+        for text in block_words:
+            # Checking if the message was sent by a moderator (because it would be nice if moderators
+            # could share links in case it would be important for them to do so).
+            if "Moderator" not in str(msg.author.roles) and text in str(msg.content.lower()):
+                await msg.delete() # Deletes the message
+                return # So that we don't continue going throuh the loop once we've already found
+                       # a blocked word
+
+        print("Not Deleting...") # This will be printed if the bot doesn't delete a message
 
 # Bot Setup
 def run_discord_bot():
@@ -46,6 +63,8 @@ def run_discord_bot():
         if message.author == client.user:
             return 
 
+        await on_offend(message, client) # call on_offend function here
+
         if message.content.startswith('?'):
             user_message = message.content[1:]
             await send_message(message, user_message, is_private=True)
@@ -53,7 +72,6 @@ def run_discord_bot():
             response = responses.get_response(message.content)
             if response: # only send a reply if the response is not empty
                 await send_message(message, response, is_private=False)
-
 
     client.run(TOKEN)
 
